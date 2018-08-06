31642238a7f6c9436a3594f71c693de4c592ced4
Adam <adamf.renaud@gmail.com>
2018-08-05 20:26:43 -0400

    Started work on the geoline class

--- Dxflib/Geometry/GeoArc.cs
+++ Dxflib/Geometry/GeoArc.cs
@@ -6,8 +6,37 @@ namespace Dxflib.Geometry
 {
     public class GeoArc : GeometricEntityBase
     {
+        private Vertex _vertex0;
+        private Vertex _vertex1;
+        private double _bulge;
 
-        public GeoArc()
+        public GeoArc(Vertex vertex0, Vertex vertex1, double bulge)
+        {
+            // Set type
+            GeometryEntityType = GeometryEntityTypes.GeoArc;
+
+            // Set Variables
+            _vertex0 = vertex0;
+            _vertex1 = vertex1;
+            _bulge = bulge;
+
+            // Subscribe to events
+            _vertex0.GeometryChanged += UpdateGeometry;
+            _vertex1.GeometryChanged += UpdateGeometry;
+
+
+            // Calculate Geometry
+
+        }
+
+        public double Length { get; private set; }
+
+        protected override double CalcLength()
+        {
+            return base.CalcLength();
+        }
+
+        private void UpdateGeometry(object sender, GeometryChangedHandlerArgs args)
         {
 
         }
