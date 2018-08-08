8f2d74863d10e4fa5ea57ee2d21c6ee63d6b9109
Adam Renaud <adamf.renaud@gmail.com>
2018-08-08 19:03:01 -0400

    Added the Vector class and asociated tests

---
+++ Dxflib.Tests/Geometry/VectorTests.cs
@@ -0,0 +1,147 @@
+﻿// Dxflib.Tests
+// VectorTests.cs
+// 
+// ============================================================
+// 
+// Created: 2018-08-08
+// Last Updated: 2018-08-08-6:57 PM
+// By: Adam Renaud
+// 
+// ============================================================
+
+using System;
+using Dxflib.Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dxflib.Tests.Geometry
+{
+    [TestClass]
+    public class VectorTests
+    {
+        /// <summary>
+        ///     Tests the basic constructor in the vector class
+        /// </summary>
+        [TestMethod]
+        public void VectorConstructor_BasicConstructor()
+        {
+            var testVector = new Vector();
+            Assert.IsTrue(Math.Abs(testVector.X - 1) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Y - 1) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Z - 1) < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests the Vertex Constructor in the vertex class
+        /// </summary>
+        [TestMethod]
+        public void VectorConstructor_VertexConstructor()
+        {
+            var vertex0 = new Vertex(0, 0);
+            var vertex1 = new Vertex(3, 4);
+            var testVector = new Vector(vertex0, vertex1);
+            Assert.IsTrue(Math.Abs(testVector.Length - 5) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.X - 3) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Y - 4) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Z - 0) < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests the x,y,z Component Constructor in the vector class
+        /// </summary>
+        [TestMethod]
+        public void VectorConstructor_ComponentConstructor()
+        {
+            var vertex0 = new Vertex(0, 0);
+            var vertex1 = new Vertex(3, 4);
+            var initalVector = new Vector(vertex0, vertex1);
+            var testVector = new Vector(initalVector.X, initalVector.Y);
+            Assert.IsTrue(Math.Abs(testVector.Length - 5) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.X - 3) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Y - 4) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(testVector.Z - 0) < GeoMath.Tolerance);
+            Assert.IsFalse(testVector.HeadVertex == vertex1);
+        }
+
+        /// <summary>
+        ///     Tests that the Geometry of the vector will update
+        ///     if the vertex of a vector is changed externally
+        /// </summary>
+        [TestMethod]
+        public void VectorUpdateGeometryTest_ChangingVertex()
+        {
+            var vertex0 = new Vertex(0, 0);
+            var vertex1 = new Vertex(3, 4);
+            var testVector = new Vector(vertex0, vertex1);
+            Assert.IsTrue(Math.Abs(testVector.Length - 5) < GeoMath.Tolerance);
+            testVector.TailVertex = new Vertex(3, 0);
+            Assert.IsTrue(Math.Abs(testVector.Length - 4) < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests that the subs to the vertex GeometryChanged Event will
+        ///     Cause the geometry of the vector to update
+        /// </summary>
+        [TestMethod]
+        public void VectorUpdateGeometryTest_ChangeVertexProperty()
+        {
+            var vertex0 = new Vertex(0, 0);
+            var vertex1 = new Vertex(3, 4);
+            var testVector = new Vector(vertex0, vertex1);
+            Assert.IsTrue(Math.Abs(testVector.Length - 5) < GeoMath.Tolerance);
+            testVector.TailVertex.X = 3;
+            Assert.IsTrue(Math.Abs(testVector.Length - 4) < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests the method that convertes the Vector to a unit vector in the direction
+        ///     of the vector
+        /// </summary>
+        [TestMethod]
+        public void VectorToUnitVectorTest()
+        {
+            var vertex0 = new Vertex(0, 0);
+            var vertex1 = new Vertex(3, 4);
+            var testVector = new Vector(vertex0, vertex1);
+            var unitVector = testVector.ToUnitVector();
+            Assert.IsTrue(Math.Abs(unitVector.Length - 1) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(unitVector.X - testVector.X / testVector.Length)
+                          < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests the Dot product functionality of the vector class
+        /// </summary>
+        [TestMethod]
+        public void VectorDotProductTests()
+        {
+            var vertex00 = new Vertex(0, 0);
+            var vertex01 = new Vertex(3, 4);
+            var testVector1 = new Vector(vertex00, vertex01);
+
+            var vertex10 = new Vertex(0, 0);
+            var vertex11 = new Vertex(1, 1);
+            var testVector2 = new Vector(vertex10, vertex11);
+
+            var dotProduct = testVector1.DotProduct(testVector2);
+            Assert.IsTrue(Math.Abs(dotProduct - 7) < GeoMath.Tolerance);
+        }
+
+        /// <summary>
+        ///     Tests the Cross product functionality of the vector class
+        /// </summary>
+        [TestMethod]
+        public void VectorCrossProductTest()
+        {
+            var vertex00 = new Vertex(0, 0);
+            var vertex01 = new Vertex(3, 4);
+            var testVector1 = new Vector(vertex00, vertex01);
+
+            var vertex10 = new Vertex(0, 0);
+            var vertex11 = new Vertex(1, 0);
+            var testVector2 = new Vector(vertex10, vertex11);
+
+            var crossProduct = testVector1.CrossProduct(testVector2);
+            Assert.IsTrue(Math.Abs(crossProduct.Length - 4) < GeoMath.Tolerance);
+        }
+    }
+}
---
+++ Dxflib/Geometry/Vector.cs
@@ -0,0 +1,175 @@
+﻿// Dxflib
+// Vector.cs
+// 
+// ============================================================
+// 
+// Created: 2018-08-08
+// Last Updated: 2018-08-08-7:02 PM
+// By: Adam Renaud
+// 
+// ============================================================
+
+using System;
+
+namespace Dxflib.Geometry
+{
+    /// <summary>
+    ///     The Vector Class: Defines vector mathematics and functionality
+    ///     for the dxf library
+    /// </summary>
+    public class Vector
+    {
+        private Vertex _vertex0;
+        private Vertex _vertex1;
+
+        /// <summary>
+        ///     Default Constructor
+        /// </summary>
+        public Vector()
+        {
+            _vertex0 = new Vertex(0, 0);
+            _vertex1 = new Vertex(1, 1, 1);
+            UpdateGeometry(this, new GeometryChangedHandlerArgs("Build"));
+            SubscribeToEvents();
+        }
+
+        /// <summary>
+        ///     PositionVector Constructor
+        /// </summary>
+        /// <param name="startingVertex">The Starting Vertex for the position vector</param>
+        /// <param name="endingVertex">The Ending Vertex for the position vector</param>
+        public Vector(Vertex startingVertex, Vertex endingVertex)
+        {
+            _vertex0 = startingVertex;
+            _vertex1 = endingVertex;
+            UpdateGeometry(this, new GeometryChangedHandlerArgs("Build"));
+            SubscribeToEvents();
+        }
+
+        /// <summary>
+        ///     UnitVector Constructor
+        /// </summary>
+        /// <param name="xComponent">The x component of a unit vector</param>
+        /// <param name="yComponent">The y component of a unit vector</param>
+        /// <param name="zComponent">The z component of a unit vector</param>
+        public Vector(double xComponent, double yComponent, double zComponent = 0)
+        {
+            _vertex0 = new Vertex(0, 0);
+            _vertex1 = new Vertex(xComponent, yComponent, zComponent);
+            UpdateGeometry(this, new GeometryChangedHandlerArgs("Build"));
+            SubscribeToEvents();
+        }
+
+        /// <summary>
+        ///     The vector's x component
+        /// </summary>
+        public double X { get; private set; }
+
+        /// <summary>
+        ///     The vector's y component
+        /// </summary>
+        public double Y { get; private set; }
+
+        /// <summary>
+        ///     The vector's z component
+        /// </summary>
+        public double Z { get; private set; }
+
+        /// <summary>
+        ///     The vector's lenght or magnitude
+        /// </summary>
+        public double Length { get; private set; }
+
+        /// <summary>
+        ///     The Head of the vector or the ending vector (Point of Application)
+        /// </summary>
+        public Vertex HeadVertex
+        {
+            get => _vertex1;
+            set
+            {
+                _vertex1 = value;
+                OnGeometryChanged(this, EventArgs.Empty);
+                UpdateGeometry(this, new GeometryChangedHandlerArgs(1));
+            }
+        }
+
+        /// <summary>
+        ///     The Tail of the vector: where the vector originates
+        /// </summary>
+        public Vertex TailVertex
+        {
+            get => _vertex0;
+            set
+            {
+                _vertex0 = value;
+                OnGeometryChanged(this, EventArgs.Empty);
+                UpdateGeometry(this, new GeometryChangedHandlerArgs(0));
+            }
+        }
+
+        /// <summary>
+        ///     The event when ever one of the vertexes change or if one of the components change
+        /// </summary>
+        public event EventHandler GeometryChanged;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="args">Some arguments</param>
+        private void UpdateGeometry(object sender, GeometryChangedHandlerArgs args)
+        {
+            X = _vertex1.X - _vertex0.X;
+            Y = _vertex1.Y - _vertex0.Y;
+            Z = _vertex1.Z - _vertex0.Z;
+            Length = GeoMath.Distance(_vertex0, _vertex1);
+        }
+
+        /// <summary>
+        ///     Subscribing to vertex events
+        /// </summary>
+        private void SubscribeToEvents()
+        {
+            _vertex0.GeometryChanged += UpdateGeometry;
+            _vertex1.GeometryChanged += UpdateGeometry;
+        }
+
+        /// <summary>
+        ///     On Geometry Changed invokation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnGeometryChanged(object sender, EventArgs args) { GeometryChanged?.Invoke(sender, args); }
+
+        /// <summary>
+        ///     Creates a new vector that is the unit vector of this vector
+        /// </summary>
+        /// <returns>A new Vector object</returns>
+        public Vector ToUnitVector()
+        {
+            return new Vector(X / Length,
+                Y / Length,
+                Z / Length);
+        }
+
+        /// <summary>
+        ///     The Mathematical Dot Product of two vectors
+        /// </summary>
+        /// <param name="other">The other vector in the dot product equation</param>
+        /// <returns>A double value that is the dot product of other and this vector</returns>
+        public double DotProduct(Vector other) { return other.X * X + other.Y * Y + other.Z * Z; }
+
+        /// <summary>
+        ///     The Cross product of this vector and the other vector (ThisVector x OtherVector)
+        /// </summary>
+        /// <param name="other">The other vector that is in the cross function</param>
+        /// <returns>A new vector that is the resultant of the cross product</returns>
+        public Vector CrossProduct(Vector other)
+        {
+            return new Vector(
+                Y * other.Z - Z * other.Y,
+                -( X * other.Z - other.X * Z ),
+                X * other.Y - other.X * Y);
+        }
+    }
+}
