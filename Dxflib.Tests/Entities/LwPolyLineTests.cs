9e778a7e01979ccf0fbb5d0563d02cb3e71f6247
Adam Renaud <adamf.renaud@gmail.com>
2018-08-07 21:29:46 -0400

    Adding some tests: Note that some tests are failing here

--- Dxflib.Tests/Entities/LwPolyLineTests.cs
+++ Dxflib.Tests/Entities/LwPolyLineTests.cs
@@ -97,5 +97,89 @@ namespace Dxflib.Tests.Entities
             Debug.WriteLine($"Total Area of Polyline is: {polylines[1].Area}");
             Assert.IsTrue(Math.Abs(polylines[1].Area - 27.0785) < GeoMath.Tolerance);
         }
+
+        [TestMethod]
+        public void LengthAndAreaTest_0Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[0].Area - 131.6307) < GeoMath.Tolerance, 
+                $"Area: {polylines[0].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[0].Length - 44.1223) < GeoMath.Tolerance,
+                $"Length: {polylines[0].Length}");
+        }
+
+        [TestMethod]
+        public void LengthAndAreaTest_1Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[1].Area - 126.0766) < GeoMath.Tolerance, 
+                $"Area: {polylines[1].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[1].Length - 44.3695) < GeoMath.Tolerance,
+                $"Length: {polylines[1].Length}");
+        }
+
+        [TestMethod]
+        public void LengthAndAreaTest_2Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[2].Area - 129.7362) < GeoMath.Tolerance, 
+                $"Area: {polylines[2].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[2].Length - 44.8019) < GeoMath.Tolerance,
+                $"Length: {polylines[2].Length}");
+        }
+
+        [TestMethod]
+        public void LengthAndAreaTest_3Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[3].Area - 125.0200) < GeoMath.Tolerance, 
+                $"Area: {polylines[3].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[3].Length - 45.1231) < GeoMath.Tolerance,
+                $"Length: {polylines[3].Length}");
+        }
+
+        [TestMethod]
+        public void LengthAndAreaTest_4Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[4].Area - 128.0246) < GeoMath.Tolerance, 
+                $"Area: {polylines[4].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[4].Length - 45.6533) < GeoMath.Tolerance,
+                $"Length: {polylines[4].Length}");
+        }
+
+        [TestMethod]
+        public void LengthAndAreaTest_5Bulge()
+        {
+            var dxfFile 
+                = new DxfFile(@"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\BulgeAreaAndLengthTests.dxf");
+            var polylines = dxfFile.GetEntitiesByType<LwPolyLine>(EntityTypes.Lwpolyline);
+            // Area
+            Assert.IsTrue(Math.Abs(polylines[5].Area - 128.0246) < GeoMath.Tolerance, 
+                $"Area: {polylines[5].Area}");
+            // Length
+            Assert.IsTrue(Math.Abs(polylines[5].Length - 45.6533) < GeoMath.Tolerance,
+                $"Length: {polylines[5].Length}");
+        }
     }
 }
