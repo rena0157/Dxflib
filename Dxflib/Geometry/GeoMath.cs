deac5373188678e3f2ee074ce44ad6a1b1350a6b
Adam Renaud <adamf.renaud@gmail.com>
2018-08-08 19:02:44 -0400

    Fixed a bug in Geomath that was not calcuating the distance for z values properly

--- Dxflib/Geometry/GeoMath.cs
+++ Dxflib/Geometry/GeoMath.cs
@@ -48,8 +48,8 @@ namespace Dxflib.Geometry
         public static double Distance(Vertex v0, Vertex v1)
         {
             return Math.Sqrt(Math.Pow(v1.X - v0.X, 2) +
-                             Math.Pow(v1.Y - v0.Y, 2)) +
-                   Math.Pow(v0.Z - v1.Z, 2);
+                             Math.Pow(v1.Y - v0.Y, 2) +
+                   Math.Pow(v0.Z - v1.Z, 2));
         }
 
         /// <summary>
