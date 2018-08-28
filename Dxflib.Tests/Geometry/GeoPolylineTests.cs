c75cb5c0c43571258173e4af60e6316224fce980
Adam Renaud <adamf.renaud@gmail.com>
2018-08-27 20:39:35 -0400

    Fixed some failing tests and added some more funcitonality to the geoPolyline Class as well as added some tests for the class

--- Dxflib.Tests/Geometry/GeoPolylineTests.cs
+++ Dxflib.Tests/Geometry/GeoPolylineTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Dxflib.Geometry;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dxflib.Tests.Geometry
@@ -6,9 +7,48 @@ namespace Dxflib.Tests.Geometry
     [TestClass]
     public class GeoPolylineTests
     {
+        // This file contains all of the information
+        // regarding the geometry used below
+        // @"C:\Dev\Dxflib\Dxflib.Tests\DxfTestFiles\GeoPolylineTests.dxf";
         [TestMethod]
-        public void TestMethod1()
+        public void GenericGeoPolylineTest_1Arc()
         {
+            // Line 0
+            var v0 = new Vertex(3.5, 0);
+            var v1 = new Vertex(6, 0);
+            var l0 = new GeoLine(v0, v1);
+            Assert.IsTrue(Math.Abs(l0.Length - 2.5) < GeoMath.Tolerance);
+
+            // Line 1
+            var v2 = new Vertex(6, 2.5);
+            var l1 = new GeoLine(v1, v2);
+            Assert.IsTrue(Math.Abs(l1.Length - 2.5) < GeoMath.Tolerance);
+
+            // Arc
+            var centerPoint = new Vertex(4.75, 1.75);
+            var startAngle = GeoMath.DegToRad(30.964);
+            var endAngle = GeoMath.DegToRad(149.036);
+            const double radius = 1.4577;
+            var arc0 = new GeoArc(centerPoint, startAngle, endAngle, radius);
+            Assert.IsTrue(Math.Abs(arc0.Length - 3.0040) < GeoMath.Tolerance);
+
+            // Line 2
+            var v3 = new Vertex(3.5, 2.5);
+            var l2 = new GeoLine(v3, v0);
+            Assert.IsTrue(Math.Abs(l2.Length - 2.5) < GeoMath.Tolerance);
+
+            // GeoPolyline
+            var geoPolyline = new GeoPolyline(); // Initialized
+
+            // Adding sections to the GeoPolyline
+            geoPolyline.Add(l0);
+            geoPolyline.Add(l1);
+            geoPolyline.Add(arc0);
+            geoPolyline.Add(l2);
+
+            // Assert
+            Assert.IsTrue(Math.Abs(geoPolyline.Length - 10.5040) < GeoMath.Tolerance);
+            Assert.IsTrue(Math.Abs(geoPolyline.Area - 7.5021) < GeoMath.Tolerance);
         }
     }
 }
--- Dxflib/Geometry/GeoPolyline.cs
+++ Dxflib/Geometry/GeoPolyline.cs
@@ -4,7 +4,7 @@
 // ============================================================
 // 
 // Created: 2018-08-07
-// Last Updated: 2018-08-07-11:36 AM
+// Last Updated: 2018-08-27-8:26 PM
 // By: Adam Renaud
 // 
 // ============================================================
@@ -18,13 +18,13 @@ namespace Dxflib.Geometry
 {
     /// <inheritdoc />
     /// <summary>
-    /// The GeoPolyline Class, which is a container of the GeoMetricEntityBase.
-    /// This Object contains both GeoArcs and GeoLines
+    ///     The GeoPolyline Class, which is a container of the GeoMetricEntityBase.
+    ///     This Object contains both GeoArcs and GeoLines
     /// </summary>
     public class GeoPolyline : GeometricEntityBase
     {
         /// <summary>
-        /// The Main GeoPolyLine Constructor
+        ///     The Main GeoPolyLine Constructor
         /// </summary>
         /// <param name="xValues">X Coordinates</param>
         /// <param name="yValues">Y Coordinates</param>
@@ -34,7 +34,7 @@ namespace Dxflib.Geometry
             List<double> yValues, List<double> bulgeList, bool polylineFlag)
         {
             // Throw Exception if the number of vertices does not match the 
-            if (xValues.Count != yValues.Count)
+            if ( xValues.Count != yValues.Count )
                 throw new EntityException("There must be a matching number of vertices");
 
             // Initialize and preallocate the vertex list
@@ -50,13 +50,13 @@ namespace Dxflib.Geometry
                 => new Vertex(t, yValues[vertexIndex])));
 
             // Iterate through all of the vertices and build either GeoArcs or GeoLines
-            for (var vertexIndex = 0; vertexIndex < Vertices.Count; ++vertexIndex)
+            for ( var vertexIndex = 0; vertexIndex < Vertices.Count; ++vertexIndex )
             {
                 var currentVertex = Vertices[vertexIndex];
                 var nextVertex
                     = vertexIndex == Vertices.Count - 1 ? Vertices[0] : Vertices[vertexIndex + 1];
 
-                if (Math.Abs(bulgeList[vertexIndex] - Bulge.BulgeNull) > GeoMath.Tolerance)
+                if ( Math.Abs(bulgeList[vertexIndex] - Bulge.BulgeNull) > GeoMath.Tolerance )
                 {
                     var geoArc = new GeoArc(currentVertex, nextVertex, bulgeList[vertexIndex]);
                     geoArc.GeometryChanged += UpdateGeometry;
@@ -70,7 +70,7 @@ namespace Dxflib.Geometry
                 }
 
 
-                if (!polylineFlag)
+                if ( !polylineFlag )
                     SectionList.RemoveAt(SectionList.Count - 1);
             }
 
@@ -78,7 +78,7 @@ namespace Dxflib.Geometry
         }
 
         /// <summary>
-        /// Default Constructor
+        ///     Default Constructor
         /// </summary>
         public GeoPolyline()
         {
@@ -86,41 +86,70 @@ namespace Dxflib.Geometry
             Vertices = new List<Vertex>();
             Length = 0;
             Area = 0;
+            IsCounterClockWise = true;
         }
 
         /// <summary>
-        /// The Sections
+        ///     The Sections
         /// </summary>
         private List<GeometricEntityBase> SectionList { get; }
 
         /// <summary>
-        /// The Vertices
+        ///     The Vertices
         /// </summary>
         private List<Vertex> Vertices { get; }
 
         /// <summary>
-        /// The Total Length of the Polyline
+        ///     The Total Length of the Polyline
         /// </summary>
         public double Length { get; private set; }
 
         /// <summary>
-        /// The Total Area of the polyline
+        ///     The Total Area of the polyline
         /// </summary>
         public double Area { get; private set; }
 
         /// <summary>
-        /// Add a section to the geo polyline
+        ///     True: The GeoPolyline was drawn counter clockwise
+        /// </summary>
+        private bool IsCounterClockWise { get; set; }
+
+        /// <summary>
+        ///     Add a section to the geo polyline
         /// </summary>
         /// <param name="section"></param>
         public void Add(GeometricEntityBase section)
         {
-            SectionList.Add(section);
-            UpdateGeometry(this, new GeometryChangedHandlerArgs("Update"));
+            // if the section is the right type and
+            // is connected to the other sections
+            switch ( section.GeometryEntityType )
+            {
+                case GeometryEntityTypes.Vertex:
+                    throw new ArgumentException("The Argument Provided should be either a " +
+                                                $"{typeof(GeoLine)} or a {typeof(GeoArc)} not " +
+                                                $"a {typeof(Vertex)}");
+                case GeometryEntityTypes.GeoLine:
+                    // Todo: Add Exception Throw if section is out of range
+                    SectionList.Add(section);
+                    IsCounterClockWise = IsCounterClockWiseCalc();
+                    Length += ( (GeoLine) section ).Length;
+                    Area += ( (GeoLine) section ).Area;
+                    break;
+                case GeometryEntityTypes.GeoArc:
+                    // Todo: Add Exception Throw if section is out of range
+                    SectionList.Add(section);
+                    IsCounterClockWise = IsCounterClockWiseCalc();
+                    Length += ( (GeoArc) section ).Length;
+                    Area += CalcAreaGeoArc((GeoArc) section);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         /// <inheritdoc />
         /// <summary>
-        /// Override of the Update Geometry method from the GeometricEntityBase class
+        ///     Override of the Update Geometry method from the GeometricEntityBase class
         /// </summary>
         /// <param name="sender">The object sender</param>
         /// <param name="args">The Arguments</param>
@@ -128,26 +157,27 @@ namespace Dxflib.Geometry
         {
             Length = CalcLength();
             Area = CalcArea();
+            IsCounterClockWise = IsCounterClockWiseCalc();
         }
 
         /// <inheritdoc />
         /// <summary>
-        /// The CalcLength function that calculates the total length of the polyline
+        ///     The CalcLength function that calculates the total length of the polyline
         /// </summary>
         /// <returns>The Total length of all sections</returns>
         protected sealed override double CalcLength()
         {
             var sumLength = 0.0;
-            foreach (var entity in SectionList)
-                switch (entity.GeometryEntityType)
+            foreach ( var entity in SectionList )
+                switch ( entity.GeometryEntityType )
                 {
                     case GeometryEntityTypes.Vertex:
                         break;
                     case GeometryEntityTypes.GeoLine:
-                        sumLength += ((GeoLine) entity).Length;
+                        sumLength += ( (GeoLine) entity ).Length;
                         break;
                     case GeometryEntityTypes.GeoArc:
-                        sumLength += ((GeoArc) entity).Length;
+                        sumLength += ( (GeoArc) entity ).Length;
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -156,11 +186,14 @@ namespace Dxflib.Geometry
             return sumLength;
         }
 
+        /// <summary>
+        ///     Function that Calculates the total area of the GeoPolyline
+        /// </summary>
+        /// <returns>The Total Area</returns>
         private double CalcArea()
         {
             var sumArea = 0.0;
             foreach ( var entity in SectionList )
-            {
                 switch ( entity.GeometryEntityType )
                 {
                     case GeometryEntityTypes.Vertex:
@@ -169,39 +202,111 @@ namespace Dxflib.Geometry
                         sumArea += ( (GeoLine) entity ).Area;
                         break;
                     case GeometryEntityTypes.GeoArc:
-                        if (SubtractBulge(((GeoArc)entity).BulgeValue))
-                            sumArea -= ( (GeoArc) entity ).Area;
-                        else
-                            sumArea += ( (GeoArc) entity ).Area;
-                        var line = new GeoLine(( (GeoArc) entity ).Vertex0,
-                            ( (GeoArc) entity ).Vertex1);
-                        sumArea += line.Area;
+                        sumArea += CalcAreaGeoArc((GeoArc) entity);
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-            }
 
             return sumArea;
         }
 
         /// <summary>
-        /// Private Function that is used to determine
-        /// if a bulge should be subtracted or added to an arc
+        /// Function that calculates the total are of an arc including the
+        /// area that is occupied by a trapezoid under the arc
+        /// </summary>
+        /// <param name="arc">The arc</param>
+        /// <returns>The Area of the Arc and Trapezoid</returns>
+        private double CalcAreaGeoArc(GeoArc arc)
+        {
+            var sumArea = 0.0;
+            // Determine if the function should subtract the 
+            // bulge
+            if ( SubtractBulge(arc.BulgeValue) )
+                sumArea -= arc.Area;
+            else
+                sumArea += arc.Area;
+
+            // Determine if the function should subtract the line
+            // that is underneath the bulge as well
+            GeoLine line;
+            if ( IsCounterClockWise )
+                line = new GeoLine(arc.Vertex1,
+                    arc.Vertex0);
+            else
+                line = new GeoLine(arc.Vertex0,
+                    arc.Vertex1);
+            sumArea += line.Area;
+
+            return sumArea;
+        }
+
+        /// <summary>
+        ///     Private Function that is used to determine
+        ///     if a bulge should be subtracted or added to an arc
         /// </summary>
         /// <param name="bulge">The Bulge Value</param>
+        /// <remarks>
+        ///     Will always return false if the number of sections
+        ///     is less than 2.
+        ///     How this function works:
+        ///     This function uses the cross product of two vectors vec0 and vec1
+        ///     to determine the draw direction of the GeoPolyline. Then with the bulge
+        ///     the following Possibilities will follow with (XProduct * BulgeValue)
+        ///     1. Bulge +ve | XProd.Z -ve: Subtract the Bulge (ie true)
+        ///     2. Bulge +ve | XProd.Z +ve: Add the Bulge (ie false)
+        ///     3. Bulge -ve | XProd.Z -ve: Add the Bulge (ie false)
+        ///     4. Bulge -ve | XProd.Z +ve: Subtract the Bulge (ie true)
+        /// </remarks>
         /// <returns>True: The bulge should be subtracted</returns>
         private bool SubtractBulge(double bulge)
         {
+            // If there is less than 2 sections return false
             if ( SectionList.Count < 2 )
                 return false;
-            var vec0 = SectionList[0].GeometryEntityType == GeometryEntityTypes.GeoArc 
-                ? ((GeoArc) SectionList[0]).ToVector() : ((GeoLine) SectionList[0]).ToVector();
 
-            var vec1 = SectionList[1].GeometryEntityType == GeometryEntityTypes.GeoArc 
-                ? ((GeoArc) SectionList[1]).ToVector() : ((GeoLine) SectionList[1]).ToVector();
+            // A vector that starts at section0 v0 and ends at section0 v1
+            var vec0 = SectionList[0].GeometryEntityType == GeometryEntityTypes.GeoArc
+                ? ( (GeoArc) SectionList[0] ).ToVector()
+                : ( (GeoLine) SectionList[0] ).ToVector();
 
+            // A vector that starts at section1 v0 and ends at section1 v1
+            var vec1 = SectionList[1].GeometryEntityType == GeometryEntityTypes.GeoArc
+                ? ( (GeoArc) SectionList[1] ).ToVector()
+                : ( (GeoLine) SectionList[1] ).ToVector();
+
+            // See possibilities in the XML remarks comments above the function
             return vec0.CrossProduct(vec1).Z * bulge < 0;
+        }
+
+        /// <summary>
+        ///     Private function that returns true if the GeoPolyline is counter clockwise
+        /// </summary>
+        /// <remarks>
+        ///     Will always return false if the number of sections is less than 2
+        /// </remarks>
+        /// <returns>True if the GeoPolyline is Counter Clockwise</returns>
+        private bool IsCounterClockWiseCalc()
+        {
+            // If the section has only 1 argument then 
+            // it cannot be counter clockwise or not
+            if ( SectionList.Count < 2 )
+                return false;
+
+            // A vector that starts at section0 v0 and ends at section0 v1
+            var vec0 = SectionList[0].GeometryEntityType == GeometryEntityTypes.GeoArc
+                ? ( (GeoArc) SectionList[0] ).ToVector()
+                : ( (GeoLine) SectionList[0] ).ToVector();
+
+            // A vector that starts at section1 v0 and ends at section1 v1
+            var vec1 = SectionList[1].GeometryEntityType == GeometryEntityTypes.GeoArc
+                ? ( (GeoArc) SectionList[1] ).ToVector()
+                : ( (GeoLine) SectionList[1] ).ToVector();
+
+            // The cross product of the two vectors vec0 X vec1
+            // will be positive if the sections were drawn counter clockwise
+            // as per the right hand rule
+            return vec0.CrossProduct(vec1).Z > 0;
         }
     }
 }
