4b8f915c3fda47346bb8fbf4faa56fa031a03272
Adam Renaud <adamf.renaud@gmail.com>
2018-09-02 10:35:22 -0400

    Organizing the Text Constructor from a TextBuffer

--- Dxflib/Entities/Text/Text.cs
+++ Dxflib/Entities/Text/Text.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Dxflib
+// Text.cs
+// 
+// ============================================================
+// 
+// Created: 2018-09-01
+// Last Updated: 2018-09-02-10:34 AM
+// By: Adam Renaud
+// 
+// ============================================================
+
 using Dxflib.Geometry;
 
 namespace Dxflib.Entities.Text
 {
+    /// <inheritdoc cref="Entity" />
+    /// <inheritdoc cref="IText" />
+    /// <summary>
+    /// </summary>
     public class Text : Entity, IText
     {
-        public Text(TextBuffer tb)
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="tb"></param>
+        public Text(TextBuffer tb) : base(tb)
         {
-            
+            Contents = tb.Contents;
+            TextStyle = tb.TextStyle;
+            IsAnnotative = tb.IsAnnotative;
+            Justify = tb.Justify;
+            Height = tb.Height;
+            Rotation = tb.Rotation;
+            WidthFactor = tb.WidthFactor;
+            Obliquing = tb.Obliquing;
+            PositionVertex = tb.PositionVertex;
         }
 
         public string Contents { get; set; }
