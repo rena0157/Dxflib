bace9f83e8b076baaef0be9bc7610b8891dd9b88
Adam Renaud <adamf.renaud@gmail.com>
2018-09-03 12:27:23 -0400

    Adding the Circle Class

---
+++ Dxflib/Entities/Circle.cs
@@ -0,0 +1,48 @@
+﻿// Dxflib
+// Circle.cs
+// 
+// ============================================================
+// 
+// Created: 2018-09-03
+// Last Updated: 2018-09-03-12:27 PM
+// By: Adam Renaud
+// 
+// ============================================================
+
+using Dxflib.Geometry;
+
+namespace Dxflib.Entities
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Circle Entity
+    /// </summary>
+    public class Circle : Entity
+    {
+        /// <inheritdoc />
+        /// <summary>
+        ///     Circle Buffer Constructor
+        /// </summary>
+        /// <param name="cb">A <see cref="CircularArcBuffer" /> Buffer</param>
+        /// <remarks>
+        ///     Note that only the Radius and CenterPoint information will be used from the
+        ///     Buffer
+        /// </remarks>
+        public Circle(CircularArcBuffer cb) : base(cb)
+        {
+            EntityType = typeof(Circle);
+            CenterPoint = new Vertex(cb.CenterPointX, cb.CenterPointY, cb.CenterPointZ);
+            Radius = cb.Radius;
+        }
+
+        /// <summary>
+        ///     Center Point of the Circle
+        /// </summary>
+        public Vertex CenterPoint { get; set; }
+
+        /// <summary>
+        ///     The Radius of the Circle
+        /// </summary>
+        public double Radius { get; set; }
+    }
+}
